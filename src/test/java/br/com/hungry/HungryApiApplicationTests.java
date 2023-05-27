79501695c5ad14a2d34f9d572036dc7329ec578d
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-05-27 17:50:51 -0300

    Projeto inicial

---
+++ src/test/java/br/com/hungry/HungryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
