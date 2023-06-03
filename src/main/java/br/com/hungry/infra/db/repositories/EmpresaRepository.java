cdf8ac6f30b7c3f9f3e2f5a42e9d57562f7bbb0f
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2023-06-02 23:17:59 -0300

    feat: CRUD empresas

--- src/main/java/br/com/hungry/infra/db/repositories/EmpresaRepository.java
+++ src/main/java/br/com/hungry/infra/db/repositories/EmpresaRepository.java
@@ -4,4 +4,7 @@ import br.com.hungry.infra.db.models.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    boolean existsByCnpj(Long cnpj);
+
 }
